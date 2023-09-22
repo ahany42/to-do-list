098037fd30efdc5b6408b8b3a416b4c29b4403cb
Ahany42 <ahanyfathy42@gmail.com>
2023-09-22 11:34:05 +0300

    added tasks saved to local storage

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var input = document.querySelector(".inputtext");
 var addbtn = document.querySelector(".addbtn");
 var taskcontainer = document.getElementById("tasks");
 var taskarr = [];
+// window.localStorage.clear();
+if(window.localStorage.getItem("tasks")){
+   taskarr = JSON.parse(window.localStorage.getItem("tasks"));
+   addelements(taskarr);
+}
 
 function addfn(){
   if(input.value !== "")
 { 
   tasktoarr(input.value);
+  addelements(taskarr);
+
   input.value=" ";
 } 
 else{
@@ -14,19 +21,40 @@ else{
 }
 }
 function tasktoarr(taskbody){
-    // window.sessionStorage.setItem("input",input.value);
-    // window.localStorage.task=input.value;
-    // const newdiv=document.createElement("div");
-    //  document.getElementById("tasks").appendChild(newdiv);
-    //  newdiv.innerText=window.localStorage.task;
     const task = {
       id:Date.now(),
       body:taskbody,
     };
+    if(task.body!==" ")
     taskarr.push(task);
-    console.log(taskarr);
+    
     }
+  function addelements(taskarr){
+    taskcontainer.innerHTML=" ";
+    taskarr.forEach((task)=>{
+      if(task.body !==" "){
+    const newdiv=document.createElement("div");
+     document.getElementById("tasks").appendChild(newdiv);
+     newdiv.innerText=task.body;
+     taskcontainer.setAttribute("task-id",task.id);
+     let deletebtn = document.createElement("p");
+     deletebtn.className="del";
+     deletebtn.innerHTML="delete";
+     newdiv.appendChild(deletebtn);
+     save(taskarr);
+      }
+    })
+
+  
+    
+  }
+  function save(taskarr){
+      console.log(input.value);
+      window.localStorage.setItem("tasks",JSON.stringify(taskarr));
+          }
+
   addbtn.addEventListener("click",addfn);
+
 
 
 
