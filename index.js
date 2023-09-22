cc715a7c3bf6cae8a15f0e05932bab85aa6254f0
Ahany42 <ahanyfathy42@gmail.com>
2023-09-22 09:29:25 +0300

    add task to array of objects

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 var input = document.querySelector(".inputtext");
 var addbtn = document.querySelector(".addbtn");
-// var delete = document.querySelectorAll(".");
-function addfn(){
-    window.sessionStorage.setItem("input",input.value);
-    window.localStorage.task=input.value;
-    const newdiv=document.createElement("div");
-     document.getElementById("tasks").appendChild(newdiv);
-     newdiv.innerText=window.localStorage.task;
-    }
+var taskcontainer = document.getElementById("tasks");
+var taskarr = [];
 
+function addfn(){
+  if(input.value !== "")
+{ 
+  tasktoarr(input.value);
+  input.value=" ";
+} 
+else{
+
+}
+}
+function tasktoarr(taskbody){
+    // window.sessionStorage.setItem("input",input.value);
+    // window.localStorage.task=input.value;
+    // const newdiv=document.createElement("div");
+    //  document.getElementById("tasks").appendChild(newdiv);
+    //  newdiv.innerText=window.localStorage.task;
+    const task = {
+      id:Date.now(),
+      body:taskbody,
+    };
+    taskarr.push(task);
+    console.log(taskarr);
+    }
   addbtn.addEventListener("click",addfn);
 
 
 
 
-// delete.addEventListener("click",(e)=>{
-//     window.localStorage.clear();
-// })
